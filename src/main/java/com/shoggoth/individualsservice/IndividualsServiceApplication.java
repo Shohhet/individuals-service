73ab0f20ba107f001d7f770e37f35cacaf4f6064
shohhet <shohhet@gmail.com>
2025-01-11 20:03:45 +0700

    init project

---
+++ src/main/java/com/shoggoth/individualsservice/IndividualsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shoggoth.individualsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndividualsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndividualsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shoggoth/individualsservice/IndividualsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoggoth.individualsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividualsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
